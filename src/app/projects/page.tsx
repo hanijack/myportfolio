953cf676c083c09169e1f61d1fa36c71c82a032f
hanijack <hani.darklt@gmail.com>
2024-05-02 10:57:51 +0300

    final commit

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -3,7 +3,8 @@ import Link from "next/link"
 
 
 
-
+export const dynamic = "force-dynamic";
+export const fetchCache = "force-no-store";
 
 const fetchedProjects = async () => {
   const response = await fetch("http://localhost:3000/api/projects", {
@@ -11,7 +12,6 @@ const fetchedProjects = async () => {
     headers: {
       "Content-Type": "application/json"
     }
-    , cache: "no-cache"
   })
 
   if (!response) {
@@ -29,10 +29,10 @@ async function Projects() {
       <section className="mb-24 flex flex-col  gap-y-4 gap-x-10 bg-[#282c34] p-4 rounded-md">
         <h1 className='text-white text-2xl  text-center'>Projects </h1>
         <div className="cont ">
-          {projects.map((proj: any) => <div key={proj._id} className="proj ">
+          {projects?.map((proj: any | null) => <div key={proj?._id} className="proj ">
             <Link  href={`./projects/${proj._id}`} className="w-1/2 hover:scale-[1.05] transition ease-in-out duration-300 flex flex-col gap-2">
-              <h4 className="text-sm text-center text-white">{proj.name} </h4>
-              <Image src={proj.imgUrl} alt={proj.name} width={200} height={300} className="mx-auto  rounded-md "></Image>
+              <h4 className="text-sm text-center text-white">{proj?.name} </h4>
+              <Image src={proj?.imgUrl} alt={proj?.name} width={200} height={300} className="mx-auto  rounded-md "></Image>
             </Link>
           </div>)}
         </div>
